ff9d89c220004b7d2cb852781daa42d2b70e14c2
Lauri Ojansivu <x@xet7.org>
2017-02-12 17:32:41 +0200

    Added: Export all Rocket.Chat users username, name and email address to CSV.

---
+++ export-rocketchat-username-name-email-to-csv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# 2017-02-11 by Lauri Ojansivu (xet7)
+# License: MIT
+
+echo "1) Exporting Rocket.Chat username,name and email to users-temp.csv ..."
+mongoexport --db rocketchat --collection users --type=csv --out=users-temp.csv --fields 'username,name,emails.0.address'
+
+echo "2) Copying only those with email address from users-temp.csv to users.csv, because there's guest users etc."
+cat users-temp.csv | grep @ > users.csv
+
+echo "3) Removing temporary file users-temp.csv ."
+rm users-temp.csv
+
+echo "3) Total count of exported users:"
+wc -l users.csv
+
+echo "4) Export Done."

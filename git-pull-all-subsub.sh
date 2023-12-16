1c81db900c4dd5008255c25a8c6077811cadad36
Lauri Ojansivu <x@xet7.org>
2023-12-16 21:45:46 +0200

    Added git pull all subsub

---
+++ git-pull-all-subsub.sh
@@ -0,0 +1,9 @@
+
+# https://stackoverflow.com/questions/3497123/run-git-pull-over-all-subdirectories
+
+
+# At directory repos/orgs , pull all repos at repos/orgs/ORGNAME/REPONAME
+# orgs could be users, git clone or git ssh.
+
+
+for i in */*/.git; do ( echo $i; cd $i/..; git pull; ); done

07a92ba939090d1399213e080794c31b15b29f8d
Lauri Ojansivu <x@xet7.org>
2023-12-16 22:25:08 +0200

    Pull all in parallel.

--- git-pull-all-subsub.sh
+++ git-pull-all-subsub.sh
@@ -4,6 +4,8 @@
 
 # At directory repos/orgs , pull all repos at repos/orgs/ORGNAME/REPONAME
 # orgs could be users, git clone or git ssh.
+# & does all in parallel.
 
-
-for i in */*/.git; do ( echo $i; cd $i/..; git pull; ); done
+for i in */*/.git; do
+  ( echo $i; cd $i/..; git pull; ) &
+done
